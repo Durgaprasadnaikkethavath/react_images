6bb639264c236ce724c479049e17653160e75277
Durgaprasadnaikkethavath <kethavathdurgaprasadnaik@gmail.com>
2024-09-23 18:27:11 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <h1>welcome react Images</h1>
+      <img src="./public/ReactImages/Blog_1.jpg" alt="user images" />
+    </>
+  );
+};
+
+export default App;
